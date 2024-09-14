2b06a4ab65a21936c234edc0094e596e837fc84d
Garry Freeman <garryfreeman09@gmail.com>
2024-09-14 22:17:44 +0300

    added tasks to solve

---
+++ javascript/leetcode/100_longest-palindromic-substring.ts
@@ -0,0 +1,12 @@
+import { test } from '../tools';
+
+function longestPalindrome(s: string): string {
+  const result = '';
+
+  for () {}
+
+  return result;
+}
+
+test(() => longestPalindrome('babad'), 'bab');
+test(() => longestPalindrome('cbbd'), 'bb');
