17de1a9e555571a2d2244027bdabdce0f405fdc8
Garry Freeman <garryfreeman09@gmail.com>
2025-02-28 12:50:48 +0300

    feat: enhance test function to measure execution time

--- javascript/tools/test.ts
+++ javascript/tools/test.ts
@@ -1,5 +1,7 @@
 export function test<T>(callback: () => T, expected: T, name?: string) {
+  const start = Date.now();
   const result = callback();
+  const end = Date.now();
   let success = expected === result;
 
   if (typeof expected === 'object' && expected !== null) {
@@ -7,6 +9,7 @@ export function test<T>(callback: () => T, expected: T, name?: string) {
   }
   console.log('▼'.repeat(50));
   name && console.log('Test:', name);
+  console.log('- time:', end - start, 'ms');
   console.log('- expected:', expected);
   console.log('- result:  ', result);
   console.log(success ? '\x1b[32m%s\x1b[0m' : '\x1b[31m%s\x1b[0m', '= equal:', success);
