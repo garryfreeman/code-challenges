9ad21370c5f98900af116c51a963896cd0830238
Garry Freeman <garryfreeman09@gmail.com>
2025-02-28 11:57:20 +0300

    feat: added solution for Valid Anagram problem with examples and tests

---
+++ javascript/leetcode/014_valid_anagram.ts
@@ -0,0 +1,28 @@
+import { test } from '../tools';
+
+function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
+  const dict: Record<string, number> = {};
+
+  for (const char of s.toLowerCase()) {
+    if (dict[char]) {
+      dict[char]++;
+    } else {
+      dict[char] = 1;
+    }
+  }
+
+  for (const char of t.toLowerCase()) {
+    if (dict[char]) {
+      dict[char]--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+test(() => isAnagram('anagram', 'nagaram'), true);
+test(() => isAnagram('rat', 'car'), false);
