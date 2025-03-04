089c260ec5d9967a4624ec951064fdf343f3d993
Garry Freeman <garryfreeman09@gmail.com>
2025-03-05 00:32:18 +0300

    feat: added solution for Generate Parentheses with examples and tests

--- javascript/leetcode/904_generate-parentheses/index.ts
+++ javascript/leetcode/904_generate-parentheses/index.ts
@@ -1,6 +1,20 @@
 import { test } from '../../tools';
 
-function generateParenthesis(n: number): string[] {}
+function generateParenthesis(n: number): string[] {
+  const result: string[] = [];
+
+  const backtrack = (acc: string, o: number, c: number) => {
+    if (acc.length === n * 2) result.push(acc);
+
+    if (o < n) backtrack(acc + '(', o + 1, c + 1);
+
+    if (c > 0) backtrack(acc + ')', o, c - 1);
+  };
+
+  backtrack('', 0, 0);
+
+  return result;
+}
 
 test(() => generateParenthesis(3), ['((()))', '(()())', '(())()', '()(())', '()()()']);
 test(() => generateParenthesis(1), ['()']);
