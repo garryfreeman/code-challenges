1c95bda1405b3826a0ebf1966ebb5572939b5d28
Garry Freeman <garryfreeman09@gmail.com>
2025-03-04 16:59:26 +0300

    feat: implemented Reverse Polish Notation evaluation with tests

--- javascript/leetcode/903_evaluate-reverse-polish-notation/index.ts
+++ javascript/leetcode/903_evaluate-reverse-polish-notation/index.ts
@@ -1,7 +1,29 @@
 import { test } from '../../tools';
 
-function evalRPN(tokens: string[]): number {}
+const operations: Record<string, (a: number, b: number) => number> = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => Math.trunc(a / b),
+};
+
+function evalRPN(tokens: string[]): number {
+  const stack: number[] = [];
+
+  for (const token of tokens) {
+    if (operations[token]) {
+      const b = stack.pop()!;
+      const a = stack.pop()!;
+
+      stack.push(operations[token](a, b));
+    } else {
+      stack.push(Number(token));
+    }
+  }
+
+  return stack[0];
+}
 
 test(() => evalRPN(['2', '1', '+', '3', '*']), 9);
 test(() => evalRPN(['4', '13', '5', '/', '+']), 6);
-test(() => evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+']), 2);
+test(() => evalRPN(['10', '6', '9', '3', '+', '-11', '*', '/', '*', '17', '+', '5', '+']), 22);
