92e9b80943c172ee6a830d42debfd183178b1ad4
Garry Freeman <garryfreeman09@gmail.com>
2025-03-04 15:32:54 +0300

    feat: added solution for Min Stack with examples and tests

--- javascript/leetcode/902_min-stack/index.ts
+++ javascript/leetcode/902_min-stack/index.ts
@@ -1,13 +1,41 @@
+import { test } from '../../tools';
+
 class MinStack {
   constructor() {}
 
-  push(val: number): void {}
+  private stack: { num: number; min: number }[] = [];
 
-  pop(): void {}
+  push(val: number): void {
+    const prev = this.stack[this.stack.length - 1];
+    this.stack.push({
+      num: val,
+      min: prev?.min !== undefined && prev.min < val ? prev.min : val,
+      // min: Math.min(prev?.min ?? Number.MAX_SAFE_INTEGER, val),
+    });
+  }
 
-  top(): number {}
+  pop(): void {
+    this.stack.pop();
+  }
 
-  getMin(): number {}
+  top(): number {
+    return this.stack[this.stack.length - 1].num;
+  }
+
+  getMin(): number {
+    return this.stack[this.stack.length - 1].min;
+  }
+}
+
+{
+  const obj = new MinStack();
+  obj.push(-2);
+  obj.push(0);
+  obj.push(-3);
+  test(() => obj.getMin(), -3);
+  obj.pop();
+  test(() => obj.top(), 0);
+  test(() => obj.getMin(), -2);
 }
 
 /**
