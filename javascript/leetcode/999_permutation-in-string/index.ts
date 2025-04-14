5d7f64c73209999e92943066d50766a410c91789
Garry Freeman <garryfreeman09@gmail.com>
2025-04-14 23:53:19 +0300

    feat: added new problem: Defuse the Bomb with tests

--- javascript/leetcode/999_permutation-in-string/index.ts
+++ javascript/leetcode/999_permutation-in-string/index.ts
@@ -1,1 +1,11 @@
+import { test } from '../../tools';
+
 function checkInclusion(s1: string, s2: string): boolean {}
+
+test(() => checkInclusion('ab', 'eidbaooo'), true);
+test(() => checkInclusion('ab', 'eidboaoo'), false);
+test(() => checkInclusion('abc', 'ccccbbbbaaaa'), false);
+test(() => checkInclusion('aab', 'aba'), true);
+test(() => checkInclusion('aab', 'abb'), false);
+test(() => checkInclusion('ab', 'abab'), true);
+test(() => checkInclusion('ab', 'a'), false);
